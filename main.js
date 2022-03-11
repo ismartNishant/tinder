9a40c3870d4b23d8aed0ad6e1f850168e5b9021f
Nishant Anil Rajput <nishantrajputa11@gmail.com>
2022-03-11 21:35:13 +0530

    ui added

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+//preloader
+  $(window).on('load',function(){
+    setTimeout(function(){
+      $(".preloader").fadeOut('slow');
+    },3000)
+  })
+
 $(document).ready(() => {
     $('#hamburger-menu').click(() => {
         $('#hamburger-menu').toggleClass('active')
@@ -5,22 +12,17 @@ $(document).ready(() => {
     })
   })
   
-  //javascript for scroll to top button
-  const scrollBtn = document.querySelector(".scrollToTop-btn");
+  // //javascript for scroll to top button
+  // const scrollBtn = document.querySelector(".scrollToTop-btn");
   
-  window.addEventListener("scroll", function () {
-    scrollBtn.classList.toggle("active", window.scrollY > 0);
-  });
+  // window.addEventListener("scroll", function () {
+  //   scrollBtn.classList.toggle("active", window.scrollY > 0);
+  // });
   
-  //javascript for scroll back to top on click scroll-to-top button
-  scrollBtn.addEventListener("click", () => {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-  });
+  // //javascript for scroll back to top on click scroll-to-top button
+  // scrollBtn.addEventListener("click", () => {
+  //   document.body.scrollTop = 0;
+  //   document.documentElement.scrollTop = 0;
+  // });
   
   
-  //javascript for navigation bar effects on scroll
-  window.addEventListener("scroll", function () {
-    const header = document.querySelector("nav-wrapper");
-    header.classList.toggle('sticky', window.scrollY > 0);
-  });
